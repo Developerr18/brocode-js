77c573b7e5ec3f1314d8f681edd8999149be272d
Developerr18 <dineshnikum1@gmail.com>
2025-07-13 21:14:25 +0530

    linked

--- Arrays/script.js
+++ Arrays/script.js
@@ -6,6 +6,7 @@ fruits.forEach((fruit, index, arr) => {
 
 console.log(fruits);
 
+////////////////////////////////////////////
 const fruitsMap = ["apple", "orange", "banana", "mango"];
 
 const formatedArr = fruitsMap.map((fruit) => {
@@ -13,3 +14,13 @@ const formatedArr = fruitsMap.map((fruit) => {
 });
 
 console.log(formatedArr);
+
+///////////////////////////////////////////
+const dates = ["2025-07-13", "2025-03-12", "2025-01-17"];
+
+const formatedDate = dates.map((date) => {
+    const splitedDate = date.split("-");
+    return `${splitedDate[2]}/${splitedDate[1]}/${splitedDate[0]}`;
+});
+
+console.log(formatedDate);
---
+++ class/script.js
@@ -0,0 +1,172 @@
+// static : it's belong to class itself not to created object from class
+/*
+class MathUtil {
+    static PI = 3.14;
+
+    static getDiameter(radius) {
+        return radius * 2;
+    }
+}
+
+console.log(MathUtil.PI);
+console.log(MathUtil.getDiameter(10));
+
+//////////////////////////////////////////
+
+class User {
+    static userCount = 0;
+
+    constructor(username) {
+        this.username = username;
+        User.userCount++;
+    }
+
+    static getUserCount() {
+        console.log(`There are ${User.userCount} users online!`);
+    }
+
+    sayHello() {
+        console.log(`Hello, my username is ${this.username}`);
+    }
+}
+
+const user1 = new User("Virat");
+const user2 = new User("Rohit");
+const user3 = new User("Gill");
+const user4 = new User("Pant");
+
+console.log(user1.userCount); // undefined : belong to class, not to obj
+console.log(User.userCount); // 4
+
+console.log(user1.username); // Virat
+
+user1.sayHello(); // Hello, my username is Virat
+User.getUserCount(); // There are 4 users online!
+
+///////////////////////////////////////////
+// Inheritance
+
+class Animal {
+    alive = true;
+
+    eat() {
+        console.log(`This ${this.name} is eating`);
+    }
+
+    sleep() {
+        console.log(`This ${this.name} is sleeping`);
+    }
+}
+
+class Rabbit extends Animal {
+    name = "rabbit";
+
+    run() {
+        console.log(`${this.name} is running`);
+    }
+}
+
+class Fish extends Animal {
+    name = "fish";
+
+    swim() {
+        console.log(`${this.name} is swimming`);
+    }
+}
+
+const rabbit = new Rabbit();
+rabbit.sleep(); // This rabbit is sleeping
+rabbit.eat(); // This rabbit is eating
+
+const fish = new Fish();
+fish.swim();
+
+/////////////////////////////////////////
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    move(speed) {
+        console.log(`${this.name} moves at a speed of ${speed} kph`);
+    }
+}
+
+class Rabbit extends Animal {
+    constructor(name, age, runSpeed) {
+        super(name, age);
+        this.runSpeed = runSpeed;
+    }
+
+    run() {
+        console.log(`${this.name} can run!`);
+        super.move(this.runSpeed);
+    }
+}
+
+class Fish extends Animal {
+    constructor(name, age, swimSpeed) {
+        super(name, age);
+        this.swimSpeed = swimSpeed;
+    }
+
+    swim() {
+        console.log(`${this.name} can swim!`);
+        super.move(this.swimSpeed);
+    }
+}
+
+const rabbit = new Rabbit("rabbit", 2, 25);
+const fish = new Fish("fish", 1, 15);
+
+console.log(rabbit);
+console.log(fish);
+
+rabbit.run(30);
+fish.swim(20);
+*/
+
+////////////////////////////////////////
+// Getters and Setters
+
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    set width(newWidth) {
+        if (newWidth > 0) {
+            this._width = newWidth;
+        } else {
+            console.error("Width must be a positive number");
+        }
+    }
+
+    set height(newHeight) {
+        if (newHeight > 0) {
+            this._height = newHeight;
+        } else {
+            console.error("Height must be a positive number");
+        }
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    get area() {
+        return this._width * this._height;
+    }
+}
+
+const rectangle = new Rectangle(40, 20);
+
+console.log(rectangle.width);
+console.log(rectangle.height);
+console.log(rectangle.area);
